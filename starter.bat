250ca8f7d8994a371ffd3138243c86445c99e6a0
Nazar Bychyk <bychyk.nazar@confilogi.com>
2025-07-31 11:09:18 +0200

    Add update check for starter

--- starter.bat
+++ starter.bat
@@ -1,8 +1,21 @@
 @echo off
 
+@echo loading..
+
 if not exist node_modules (
     call npm install
 )
+
+:: check for updates
+for /f %%a in ('git ls-remote origin refs/heads/%BRANCH%') do set REMOTE=%%a
+for /f %%a in ('git rev-parse %BRANCH%') do set LOCAL=%%a
+
+if not "%LOCAL%" == "%REMOTE%" (
+    git pull
+    call npm install
+)
+
+cls
 
 call npm start
 
